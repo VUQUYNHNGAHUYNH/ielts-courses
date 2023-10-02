5677083edc0e7028c4ea756a23ec7d3470d14b93
NGA <r0917268@student.thomasmore.be>
2023-10-02 23:19:49 +0700

    analytics dashboard

---
+++ app/(dashboard)/(routes)/(root)/_components/info-card.tsx
@@ -0,0 +1,26 @@
+import { Badge } from "@/components/ui/badge";
+import { LucideIcon } from "lucide-react";
+
+interface InfoCardProps {
+  numberOfItems: number;
+  label: string;
+  icon: LucideIcon;
+}
+
+export const InfoCard = ({
+  icon: Icon,
+  numberOfItems,
+  label,
+}: InfoCardProps) => {
+  return (
+    <div className="border rounded-md flex items-center gap-x-2 p-3">
+      <Icon />
+      <div>
+        <p className="font-medium">{label}</p>
+        <p className="text-gray-500 text-sm">
+          {numberOfItems} {numberOfItems === 1 ? "Course" : "Courses"}
+        </p>
+      </div>
+    </div>
+  );
+};
